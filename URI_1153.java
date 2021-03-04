d973ed7f5993d6ba44a826400cfecae73cf26be0
Daniel Macedo <70235487+macandceara@users.noreply.github.com>
2021-03-03 21:17:45 -0300

    Update URI_1153.java
    Correção

--- URI_1153.java
+++ URI_1153.java
@@ -1,4 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
 
-public class URI_1153 {
+ 
+public class URI_1153{
+    
+    public static int fat(int n) {
+    	
 
+        if (n == 0 || n == 1) {return 1; 
+        }else {
+        	return n*fat(n-1);
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(fat(n));
+ 
+    }
+ 
 }
